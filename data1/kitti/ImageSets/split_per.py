949752efad3ef647ec8f29caf67bef723b78fb2d
li734969503 <734969503@qq.com>
2023-06-16 22:46:49 +0800

    v2.0

---
+++ data1/kitti/ImageSets/split_per.py
@@ -0,0 +1,17 @@
+import numpy as np
+import sys
+
+ratio = float(sys.argv[1])
+num = int(sys.argv[2])
+
+with open("train.txt", "r") as f:
+    lines = f.read().strip().split('\n')
+    #inds = np.random.choice(len(lines), int(len(lines)*ratio), replace=False)
+    inds = np.arange(0,len(lines), 10)
+    newlines= []
+    for i in inds:
+        newlines.append(f'{lines[i]} {i}')
+
+with open("train_%.2f_%d.txt" % (ratio, num), "w") as fw:
+    fw.write('\n'.join(newlines))
+
